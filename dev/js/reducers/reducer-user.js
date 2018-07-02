445b0aa75e309772c90c4ccba919385a96ce6750
Tecvoc <cloverr@tutanota.com>
2018-07-03 00:28:39 +0530

    add basic redux

---
+++ dev/js/reducers/reducer-user.js
@@ -0,0 +1,36 @@
+export default function() {
+    return [
+      {
+        id: 1,
+        first: "Piko",
+        last: "Papita",
+        age: 20
+      },
+      {
+        id: 2,
+        first: "Rose",
+        last: "Harvey",
+        age: 22
+      },
+    ]
+}
+// export default function () {
+//     return [
+//         {
+//             id: 1,
+//             first: "Bucky",
+//             last: "Roberts",
+//             age: 71,
+//             description: "Bucky is a React developer and YouTuber",
+//             thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+//         },
+//         {
+//             id: 2,
+//             first: "Joby",
+//             last: "Wasilenko",
+//             age: 27,
+//             description: "Joby loves the Packers, cheese, and turtles.",
+//             thumbnail: "http://i.imgur.com/52xRlm8.png"
+//         }
+//       ]
+//     };
